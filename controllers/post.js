e0b1a59a1e8665a0c014a00e78b4528eccc4d54f
ngel rojas p <aomarrojasp@gmail.com>
2020-02-07 18:47:14 -0300

    single post, postby

--- controllers/post.js
+++ controllers/post.js
@@ -1,18 +1,59 @@
-const Post = require('../models/post');
+const Post = require('../models/post')
+const formidable = require('formidable')
+const fs = require('fs')
 
-exports.getPosts =  (req, res)=>{
-    const posts = Post.find().select('_id title body')
-        .then((posts) => {
-            res.json({posts}) 
+exports.getPosts = (req, res) => {
+  const posts = Post.find()
+    .populate('postedBy', '_id name')
+    .select('_id title body')
+    .then(posts => {
+      res.json({posts})
+    })
+    .catch(err => console.log(err))
+}
+
+exports.createPost = (req, res, next) => {
+  let form = formidable.IncomingForm()
+  form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({error: 'images could not be uploaded'})
+    }
+    let post = new Post(fields)
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    post.postedBy = req.profile
+    if (files.photo) {
+      post.photo.data = fs.readFileSync(files.photo.path)
+      post.photo.contentType = files.photo.type
+    }
+    post.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
         })
-        .catch(err => console.log(err));
-};
+      }
+      res.json(result)
+    })
+  })
+  const post = new Post(req.body)
+  post.save().then(result => {
+    res.status(200).json({
+      post: result,
+    })
+  })
+}
 
-exports.createPost = (req, res) => {
-    const post = new Post(req.body);
-    post.save().then(result => {
-        res.status(200).json({
-            post: result 
-        }) 
+exports.postByUser = (req, res) => {
+  Post.find({postedBy: req.profile._id})
+    .populate('postedBy', '_id name')
+    .sort('_created')
+    .exec((err, posts) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        })
+      }
+      res.json({posts})
     })
 }
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const {getPosts, createPost} = require('../controllers/post')
+const {getPosts, createPost, postByUser} = require('../controllers/post')
 const {createPostValidator} = require('../validator')
 const {requireSignin} = require('../controllers/auth')
 const {userById} = require('../controllers/user')
 const router = express.Router()
 
 router.get('/', requireSignin, getPosts)
-router.post('/post', requireSignin, createPostValidator, createPost)
+router.post('/post/new/:userId', requireSignin, createPost, createPostValidator)
+router.get('/posts/by/:userId', requireSignin, postByUser)
+
+// any route containing :userid, our app will first execute userbyid
 router.param('userId', userById)
 
 module.exports = router
